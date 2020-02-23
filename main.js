f52f07a9932865f84ffff37022db57b69ccb34d1
Viet <vitekpham97@seznam.cz>
2020-02-23 15:55:05 +0100

    some work

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello...');
+let content = document.querySelector('h1');
+let pom = false;
+
+const action = () => {
+    if (pom === false) {
+        content.setAttribute('style','color: red;');
+        document.getElementById('list').setAttribute('style','list-style-type: none;');
+        pom = true;
+    } else if (pom === true) {
+        content.setAttribute('style','color: black;');
+        pom = false;
+    }
+}
